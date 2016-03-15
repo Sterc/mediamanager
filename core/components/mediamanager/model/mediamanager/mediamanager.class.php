07fddf413e2c928845d86cca7bd524e2858cea92
Jesse Visser <jesse@sterc.nl>
2016-03-15 15:50:00 +0100

    Add default module files

---
+++ core/components/mediamanager/model/mediamanager/mediamanager.class.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Media Manager Main Class
+ *
+ * @package mediamanager
+ */
+class MediaManager
+{
+    /**
+     * The modX object.
+     */
+    public $modx = null;
+
+    /**
+     * The namespace for this package.
+     */
+    public $namespace = 'mediamanager';
+
+    /**
+     * Holds all configs values.
+     */
+    public $config = array();
+
+    /**
+     * Initialize the class.
+     *
+     * @since    1.0.0
+     * @param    modX    $modx    The modX object.
+     * @param    array   $config  Array with config values.
+     */
+    public function __construct(modX &$modx, array $config = array()) {
+        $this->modx =& $modx;
+        $this->namespace = $this->modx->getOption('namespace', $config, 'mediamanager');
+
+        $basePath   = $this->modx->getOption('mediamanager.core_path', $config, $this->modx->getOption('core_path') . 'components/mediamanager/');
+        $assetsUrl  = $this->modx->getOption('mediamanager.assets_url', $config, $this->modx->getOption('assets_url') . 'components/mediamanager/');
+        $assetsPath = $this->modx->getOption('mediamanager.assets_path', $config, $this->modx->getOption('assets_path') . 'components/mediamanager/');
+
+        $this->config = array_merge(array(
+            'base_path'       => $basePath,
+            'core_path'       => $basePath,
+            'model_path'      => $basePath . 'model/',
+            'processors_path' => $basePath . 'processors/',
+            'elements_path'   => $basePath . 'elements/',
+            'templates_path'  => $basePath . 'templates/',
+            'assets_path'     => $assetsPath,
+            'js_url'          => $assetsUrl . 'js/',
+            'css_url'         => $assetsUrl . 'css/',
+            'assets_url'      => $assetsUrl,
+            'connector_url'   => $assetsUrl . 'connector.php',
+        ), $config);
+
+        $this->modx->addPackage('mediamanager', $this->config['model_path']);
+        $this->modx->lexicon->load('mediamanager:default');
+    }
+}
