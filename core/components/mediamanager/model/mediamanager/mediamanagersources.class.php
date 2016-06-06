5d45fcc464201bdcc418320b1e9b982fb4e1f4e6
Jesse Visser <jesse@sterc.nl>
2016-06-06 15:56:35 +0200

    Remove sources table

--- core/components/mediamanager/model/mediamanager/mediamanagersources.class.php
+++
@@ -1,2 +0,0 @@
-<?php
-class MediamanagerSources extends xPDOSimpleObject {}
--- core/components/mediamanager/model/mediamanager/mysql/mediamanagersources.class.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once (dirname(dirname(__FILE__)) . '/mediamanagersources.class.php');
-class MediamanagerSources_mysql extends MediamanagerSources {}
