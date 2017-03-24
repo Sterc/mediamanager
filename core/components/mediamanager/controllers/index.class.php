6ad4325219d1d5958f8860855679c5b1d5d15b8f
Roel Zeilstra <roel@sterc.nl>
2017-03-24 12:39:37 +0100

    Loading lexicons

--- core/components/mediamanager/controllers/index.class.php
+++ core/components/mediamanager/controllers/index.class.php
@@ -9,7 +9,8 @@ abstract class MediaManagerManagerController extends modExtraManagerController
     public function initialize()
     {
         $this->mediaManager = new MediaManager($this->modx);
-
+        $this->modx->getService('lexicon', 'modLexicon');
+        $this->modx->lexicon->load('mediamanager');
         /**
          * Add the CSS.
          */
