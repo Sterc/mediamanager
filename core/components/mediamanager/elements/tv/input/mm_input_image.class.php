7827018349a6492598ea5394dae63dc27d4b169d
Sander Drenth <sander@sterc.nl>
2021-02-05 11:50:43 +0100

    Fixing rendering of mm input tv

--- core/components/mediamanager/elements/tv/input/mm_input_image.class.php
+++ core/components/mediamanager/elements/tv/input/mm_input_image.class.php
@@ -1,34 +1,35 @@
 <?php
-if(!class_exists('MediaManagerInputRender')) {
+if (!class_exists('MediaManagerInputRender')) {
     class MediaManagerInputRender extends modTemplateVarInputRender
     {
         public function getTemplate()
         {
-            return $this->modx->getOption('mediamanager.core_path', null, $this->modx->getOption('core_path') . 'components/mediamanager/') . 'elements/tv/input/tpl/mm_input_image.tpl';
+            return $this->modx->getOption(
+                'mediamanager.core_path',
+                null,
+                $this->modx->getOption('core_path') . 'components/mediamanager/'
+            ) . 'elements/tv/input/tpl/mm_input_image.tpl';
         }
 
-        public function process($value, array $params = array())
+        public function process($value, array $params = [])
         {
             if (!is_numeric($value)) {
                 return $value;
             }
 
-            $path   = '';
-            $source = '';
-            $image  = $this->modx->getObject('MediamanagerFiles', $value);
-
-            if ($image) {
-                $path   = $image->get('path');
-                $source = $image->get('media_sources_id');
+            $path = '';
+            $file = $this->modx->getObject('MediamanagerFiles', $value);
+            if ($file) {
+                $mediaSourceId  = $file->get('media_sources_id');
+                $mediaSource    = $this->modx->getObject('sources.modFileMediaSource', ['id' => $mediaSourceId]);
+                $basePath       = $mediaSource->getProperties()['basePath']['value'];
+                $path           = $basePath . $file->get('path');
             }
 
             $this->setPlaceholder('path', $path);
-            $this->setPlaceholder('source', $source);
 
             return $value;
         }
-
     }
 }
-
 return 'MediaManagerInputRender';
