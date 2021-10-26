3463132d916399ddea92fcb60355690d030fff86
Oene Tjeerd de Bruin <oenetjeerd@sterc.nl>
2021-10-26 10:55:53 +0200

    Fixing issue #17, #18 and #19

--- assets/components/mediamanager/connector.php
+++ assets/components/mediamanager/connector.php
@@ -4,7 +4,7 @@
  *
  * @package mediamanager
  */
-require_once $_SERVER['DOCUMENT_ROOT'] . '/config.core.php';
+require_once dirname(__DIR__, 3) . '/config.core.php';
 require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
 require_once MODX_CONNECTORS_PATH . 'index.php';
 $corePath = $modx->getOption('mediamanager.core_path', null, $modx->getOption('core_path') . 'components/mediamanager/');
