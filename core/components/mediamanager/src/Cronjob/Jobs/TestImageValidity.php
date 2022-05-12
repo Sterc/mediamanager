52c865046bac2c124173e11846b5f88b99f52270
Sander Drenth <sander@sterc.nl>
2022-05-12 14:37:46 +0200

    Add mediasource name in email subject #43

--- core/components/mediamanager/src/Cronjob/Jobs/TestImageValidity.php
+++ core/components/mediamanager/src/Cronjob/Jobs/TestImageValidity.php
@@ -21,12 +21,13 @@ class TestImageValidity extends Job
     {
         if (count($this->getMediaSources()) > 0) {
             foreach ($this->getMediaSources() as $mediaSource) {
+                $this->resetItems();
 
                 $frequencies = !empty($mediaSource->getPropertyList()['mediamanagerLicenseTestFrequencies']) ? json_decode($mediaSource->getPropertyList()['mediamanagerLicenseTestFrequencies'], true) : [];
 
                 /* Set test frequencies. */
                 $this->setFrequencies($frequencies);
-            
+
                 $imageSources = !empty($mediaSource->getPropertyList()['mediamanagerLicenseSources']) ? json_decode($mediaSource->getPropertyList()['mediamanagerLicenseSources'], true) : [];
                 $this->setExpiredImageSources($imageSources);
 
@@ -35,30 +36,31 @@ class TestImageValidity extends Job
                  * Images are also marked as expired even if the image itself is not expired, but the attached source is.
                  */
                 $query = $this->modx->newQuery('MediamanagerFiles');
-                $query->leftJoin('MediamanagerFilesLicenseFile', 'MediamanagerFilesLicenseFile', 'MediamanagerFilesLicenseFile.mediamanager_files_id = MediamanagerFiles.id');
+                $query->rightJoin('MediamanagerFilesLicenseFile', 'MediamanagerFilesLicenseFile', 'MediamanagerFilesLicenseFile.mediamanager_files_id = MediamanagerFiles.id');
                 $query->leftJoin('MediamanagerFilesLicense', 'MediamanagerFilesLicense', 'MediamanagerFilesLicenseFile.license_id = MediamanagerFilesLicense.id');
-       
+
                 $frequencyDates = [];
                 foreach ($frequencies as $frequency) {
-                    $frequencyDates[] = date('Y-m-d', strtotime('+' . $frequency));                    
+                    $frequencyDates[] = date('Y-m-d', strtotime('+' . $frequency));
                 }
 
                 $where = [
-                    [  
+                    [
                         'MediamanagerFilesLicense.image_valid_enddate:<=' => date('Y-m-d 00:00:00')
                     ], [
                         'MediamanagerFilesLicense.image_valid_enddate:IN' => $frequencyDates
                     ]
                 ];
-                 
+
                 /* If expired or about to expire image sources have been found, also collect images attached to these sources. */
                 if (count($this->expiredImageSources) > 0) {
                     $where[] = [
-                        'MediamanagerFilesLicense.image_source:IN' => array_column($this->expiredImageSources, 'key')    
+                        'MediamanagerFilesLicense.image_source:IN' => array_column($this->expiredImageSources, 'key')
                     ];
                 }
 
                 $query->where($where, xPDOQuery::SQL_OR);
+                $query->where(['media_sources_id' => $mediaSource->get('id')]);
 
                 foreach ($this->modx->getIterator('MediamanagerFiles', $query) as $image) {
                     $expired         = false;
@@ -88,11 +90,11 @@ class TestImageValidity extends Job
 
                         $messages[] = $this->modx->lexicon('mediamanager.license.email.image_source_expired');
                     }
-                
+
                     if ($expired || $expiredBySource) {
                         $this->expiredItems[] = [
                             'item'          => $image->toArray(),
-                            'link'          => $this->makeManagerUrl(['a' => 'home', 'namespace' => 'mediamanager', 'file' => $image->get('id')]),
+                            'link'          => $this->makeManagerUrl(['a' => 'home', 'namespace' => 'mediamanager', 'file' => $image->get('id'), 'source' => $mediaSource->get('id')]),
                             'message'       => implode('<br/>', $messages),
                             'resources'     => implode('<br/>', $resources)
                         ];
@@ -100,7 +102,7 @@ class TestImageValidity extends Job
                         $expireDate = new DateTime($license->get('image_valid_enddate'), new DateTimeZone('UTC'));
                         $curDate    = new DateTime('now', new DateTimeZone('UTC'));
                         date_sub($curDate, date_interval_create_from_date_string('1 day'));
-            
+
                         $diff = $this->formatDateInterval($expireDate->diff($curDate));
 
                         if (isset($this->expiredImageSources[$license->get('image_source')])) {
@@ -121,10 +123,16 @@ class TestImageValidity extends Job
                 }
 
                 if ($this->shouldSendNotification()) {
-                    $recipients = !empty($mediaSource->getPropertyList()['mediamanagerLicenseTestRecipients']) ? explode(',', $mediaSource->getPropertyList()['mediamanagerLicenseTestRecipients']) : []; 
+                    $recipients = !empty($mediaSource->getPropertyList()['mediamanagerLicenseTestRecipients']) ? explode(',', $mediaSource->getPropertyList()['mediamanagerLicenseTestRecipients']) : [];
 
                     $this->setProperties([
-                        'subject'    => $this->modx->lexicon('mediamanager.license.email.image_validity.subject', ['name' => $this->modx->getOption('site_name')]),
+                        'subject'    => $this->modx->lexicon(
+                            'mediamanager.license.email.image_validity.subject',
+                            [
+                                'name'        => $this->modx->getOption('site_name'),
+                                'mediasource' => $mediaSource->get('name')
+                            ]
+                        ),
                         'tpl'        => 'emails/license/images-notification',
                         'itemTpl'    => 'emails/license/image/item',
                         'recipients' => $recipients
@@ -151,7 +159,7 @@ class TestImageValidity extends Job
         if (count($imageSources) > 0) {
             $curDate    = new DateTime('now', new DateTimeZone('UTC'));
             date_sub($curDate, date_interval_create_from_date_string('1 day'));
-            
+
             foreach ($imageSources as $imageSource) {
                 if (!empty($imageSource['expireson'])) {
                     $expireDate = new DateTime($imageSource['expireson'], new DateTimeZone('UTC'));
